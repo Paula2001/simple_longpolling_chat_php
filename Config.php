a9c5b0a0c783e810f0ce5eb9b12c1d9425c52150
Paula George <polagorge@gmail.com>
2019-01-28 21:37:01 +0200

    config your connection to the db

---
+++ Config.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Application configuration
+ *
+ * PHP version 7
+ *
+ * Paula George
+ */
+class Config
+{
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = 'chat';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = 'root';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = '';
+}
